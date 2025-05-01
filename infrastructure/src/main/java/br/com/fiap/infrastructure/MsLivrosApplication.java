0430a7a9a8fd02d74406affa660eaea5042fd4f7
luizfilipebp <luizfilipe098@gmail.com>
2025-05-01 10:46:11 -0300

    commit inicial

---
+++ infrastructure/src/main/java/br/com/fiap/infrastructure/MsLivrosApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.infrastructure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsLivrosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsLivrosApplication.class, args);
+    }
+
+}
