f8c3541e528699abe1abb64275e3441536d76968
luizfilipebp <luizfilipe098@gmail.com>
2025-05-27 22:51:54 -0300

    removido importações específicas de anotações do JPA em LivroEntity.java

--- infrastructure/src/main/java/br/com/fiap/infrastructure/entities/LivroEntity.java
+++ infrastructure/src/main/java/br/com/fiap/infrastructure/entities/LivroEntity.java
@@ -1,10 +1,7 @@
 package br.com.fiap.infrastructure.entities;
 
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
